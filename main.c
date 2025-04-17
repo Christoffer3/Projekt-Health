5adb57e70bdbd51bc079bba4a310fd9a4fa17c63
Christoffer3 <ChristofferEBohmC@Outlook.com>
2025-04-17 16:11:22 +0200

    Add files via upload
    main 1.0

--- main.c
+++ main.c
@@ -7,17 +7,17 @@ int main(void){
     int lookUpTbl[16]={1,4,7,14,2,5,8,0,3,6,9,15,10,11,12,13};
     int dac=0, speed=-100;
     int adcr, tmpr;
+    int sida=0;
     char digits[10][10]={"Zero ","One  ","Two  ","Three","Four ","Five ","Six  ","Seven","Eight","Nine "};
     char ch[]={126, 127};
     t5omsi();                               // Initialize timer5 1kHz
     colinit();                              // Initialize column toolbox
     l88init();                              // Initialize 8*8 led toolbox
     keyinit();                              // Initialize keyboard toolbox
-    Lcd_SetType(LCD_NORMAL);//INVERTED);                // or use LCD_INVERTED!
+    Lcd_SetType(LCD_NORMAL);                // or use LCD_INVERTED!
     Lcd_Init();
-    LCD_Clear(RED);
-    LCD_ShowStr(10, 10, "Icon/art template", WHITE, TRANSPARENT);
-    LCD_Fill(10,30, 20, 40, BLUE);
+    LCD_Clear(WHITE);
+    LCD_ShowStr(10, 10, "Alan Bortoleto", BLACK, TRANSPARENT);
 
 
     while (1) {
@@ -27,21 +27,40 @@ int main(void){
         if (t5expq()) {                     // Manage periodic tasks
             l88row(colset());               // ...8*8LED and Keyboard
             ms++;                           // ...One second heart beat
-            //LCD_ShowStr(10, 50, "*", WHITE, OPAQUE);
-            //LCD_DrawPoint(10,50,GREEN);
             if (ms==1000){
               ms=0;
               l88mem(0,s++);
-              //LCD_ShowStr(10, 30, digits[s%10], WHITE, OPAQUE);
-              LCD_ShowChar(10, 50, ch[0], OPAQUE, WHITE);
-              LCD_ShowChar(30, 50, ch[1], OPAQUE, WHITE);
             }
-            if ((key=keyscan())>=0) {       // ...Any key pressed?
+            if ((key=keyscan())>=0 || (key) >=0) {       // ...Any key pressed?
               if (pKey==key) c++; else {c=0; pKey=key;}
               l88mem(1,lookUpTbl[key]+(c<<4));
+              
+              if (lookUpTbl[key] == 0){     // startsida (0)
+                LCD_Clear(WHITE);
+                LCD_ShowStr(10, 10, "Alan Bortoleto", BLACK, TRANSPARENT);
+                sida=0;
+              }
+              if (lookUpTbl[key] == 1){     // sida 1 (blodtyp)
+                LCD_Clear(BLUE);
+                LCD_ShowStr(10, 10, "Alan Bortoleto", BLACK, TRANSPARENT);
+                LCD_ShowStr(10, 30, "Bloodtype: A+", BLACK, TRANSPARENT);
+                sida=1;
+              }
+              if (lookUpTbl[key] == 2){     // sida 1 (blodtyp + blodmedicin)
+                LCD_Clear(BLUE);
+                LCD_ShowStr(10, 10, "Alan Bortoleto", BLACK, TRANSPARENT);
+                LCD_ShowStr(10, 30, "Bloodtype: A+", BLACK, TRANSPARENT);
+                LCD_ShowStr(10, 50, "Med: Anticoagulant", RED, TRANSPARENT);
+                sida=1;
+              }
+              if (lookUpTbl[key] == 3){     // sida 2 (allergier)
+                LCD_Clear(RED);
+                LCD_ShowStr(35, 10, " Allergies: ", WHITE, BLACK);
+                LCD_ShowStr(10, 30, "Latex & Opioids", WHITE, TRANSPARENT);
+                LCD_ShowStr(10, 50, "Nuts & Bees", WHITE, TRANSPARENT);
+                sida=2;
+              }
             }
-            l88mem(2,idle>>8);              // ...Performance monitor
-            l88mem(3,idle); idle=0;
         }
     }
 }
