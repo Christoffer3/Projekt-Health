a372907ff9cc5613051aa8387ad7e48b021ff2fc
Christoffer3 <chbohm@kth.se>
2025-05-17 14:32:30 +0200

    Add files via upload
    Senaste versionen som fungerar för navigeringen.

--- main.c
+++ main.c
@@ -6,73 +6,70 @@
 #define EI 1
 #define DI 0
 
-// created constants by me
-#define MODESWITCH -5
+/* skapade konstanter av mig */
+#define MODESWITCH (-5)
+
 
 /* funktions deklarationer */
-void vSlide0(void);
+void vSlide0(void);           // View-mode paketet
 void vSlide1(void);
 void vSlide1Info(void);
 void vSlide2(void);
 void vSlide2Info(void);
 void vSlide3(void);
 void vSlide3Info(void);
-
-void rSlide1(void);
-//
+void rSlide1(void);           // Edit(r)-mode paketet
+//  alfabetet...
 void rSlide2(void);
-//
+//  -\\-
 void rSlide3(void);
-//
-// röd börjar
-void rSlide4(void);
-//
+//  -\\-
+void rSlide4(void);           // röda slides börjar
+//  -\\-
 void rSlide5(void);
-//
+//  -\\-
 void rSlide6(void);
-//
-void rSlide7(void);
-void rSlide7Info(void);
-void rSlide7InfoB(void);
-// orange börjar
-void rSlide8(void);
-//
+//  -\\-
+void rSlide7(void);           // in-ruta alternativet
+void rSlide7Info(void);       // infoslide txt-ruta
+void rSlide7InfoB(void);      // infoslide ut-ruta alternativet
+void rSlide8(void);           // orange slides börjar
+//  -\\-
 void rSlide9(void);
-//
+//  -\\-
 void rSlide10(void);
-//
-void rSlide11(void);
-void rSlide11Info(void);
-void rSlide11InfoB(void);
-// gul börjar
-void rSlide12(void);
-//
+//  -\\-
+void rSlide11(void);          //  -\\-
+void rSlide11Info(void);      //  -\\-
+void rSlide11InfoB(void);     //  -\\-
+void rSlide12(void);          // gula slides börjar
+// -\\-
 void rSlide13(void);
-//
+// -\\-
 void rSlide14(void);
-//
-void rSlide15(void);
-void rSlide15Info(void);
-void rSlide15InfoB(void);
+// -\\-
+void rSlide15(void);          //  -\\-
+void rSlide15Info(void);      //  -\\-
+void rSlide15InfoB(void);     //  -\\-
 
 
 
 int main(void){
-  int ms=0, s=0, key, pKey=-1, c=0, idle=0, rtc, hh, mm, ss;
+  int ms=0, s=0, key, pKey=-1, c=0, idle=0, rtc, hh, mm, ss;        // variabler från tidigare paket...
   int lookUpTbl[16]={1,4,7,14,2,5,8,0,3,6,9,15,10,11,12,13};
   int dac=0, speed=-100;
   int adcr, tmpr;
   char digits[10][10]={"Zero ","One  ","Two  ","Three","Four ","Five ","Six  ","Seven","Eight","Nine "};
 
-  /* created variables by me */
-  int mode=1, modeSwap=0;    // där om modeswap==MODESWITCH så byter mode, ska uppnås via om på case 0 o trycker upp 5 gånger men modeSwap nollställs om...
-  int vUsrPos=0;             // mode==0, sorts id för vart användaren är, varje knapptryck gör +- 1 och */ 10, så slide 0 är 0, slide 1 är 1, slide 2 inforuta är 20 och slide 3 är 3. Där varje huvslide hämtar de 3 olika textrutornas info från tilldelad flashadress, detsamma gäller infosliden som hämtar från (en? -- då endast en inforuta) flashadress.
-  int rUsrPos=1;             // mode==1, samma här men liten annan konstruktion då har flera slides, varje slide hämtar de olika textrutorna samt har en fjärde rutan per slide (utom slide 0) som är "in/ut rutan". R-mode markear dock vilken kategori user står på också. varje textruta har ju egen adressrymd. 
+  /* skapade variabler av mig */
+  int mode=0, modeSwap=0;    // om modeswap==MODESWITCH så byts mode, ska preliminärt uppnås via om usr är på case 0 o trycker upp 5 gånger.
+  int vUsrPos=0;             // mode==0, sorts id för vart användaren är, varje knapptryck gör +- 1 och */ 10. Så slide 0 är 0, slide 1 är 1, slide 2 inforuta är 20 och slide 3 är 3. Där varje huvslide hämtar de 3 olika textrutornas info från tilldelad flashadress, detsamma gäller infosliden som hämtar från (en? -- då endast en inforuta) flashadress.
+  int rUsrPos=1;             // mode==1, samma här men liten annan konstruktion (se nedan switch) då har flera slides (en för varje ruta i slide). Varje slide hämtar de olika textrutornas info från flash samt har en fjärde rutan per slide (utom slide 0) som är "in/ut rutan" -- till huv. slides tillhörande infoslide. r-mode markear dock också vilken text-kategori user står på också (varje textruta har ju egen adressrymd).
   //char vSlides [4];        // view. 4 huv. slides
-  //char vTxtBox [12];       // view. 3 textrutor  per slide (0-2 för slide 0 etc) samt en tillhörande beskrivningsslide per textruta
+  //char vTxtBox [12];       // view. 3 textrutor  per slide (0-2 för slide 0 etc)
   //char vInfoSlide [3];     // view. 3 infoslides för slide 1-3 (med en textruta respektive)
   //char rSlides[4];         // r. 4 huv. slides
-  //char rTxtBox[15];        // r. 3 + 4 + 4 + 4 rutor över de 4 slides, då första slide ej har beskrivning (därav 3 -- utan "in/ut ruta").
+  //char rTxtBox[15];        // r. 3 + 4 + 4 + 4 textrutor över de 4 slides, då första slide ej har beskrivning (därav 3 -- utan "in/ut ruta").
   //char rInfoSlideBox[6];   // r. 3 infoslides textrutor samt "in/ut ruta" (ut -- tillbaka till huv. slide)
 
 
@@ -84,11 +81,10 @@ int main(void){
   Lcd_Init();
   LCD_Clear(RED);
 
-  eclic_global_interrupt_enable();        // !!! INTERRUPT ENABLED !!!
 
 
-  /* interrupt */
-  // interrupt sker, timer startar, ser knapptrycket o gör +/- (från och med andra knapptryck!) på gällande positionsvariabel (v/r) samt lägger in i modeSwap, om modeSwap == MODESWITCH (-5) så görs mode=1 och nollställer UsrPos variablerna (2). sedan baserat på mode gör en av de två switcharna, sedan har vi en switch med varje position och funktionsanrop till sliden, default är så ifall nåogt hamnar i värde som ej finns nollställa till ursprungsvariabel (0)
+  /* Interrupt */
+  // interrupt sker och timer startar. Ser knapptrycket o gör +/- (från och med andra knapptryck!) på gällande positionsvariabel (v/r), (obs om modeSwap == MODESWITCH (-5) så görs mode=1 och nollställer UsrPos variablerna (2)). Sedan baserat på mode gör en av de två switcharna, sedan har vi en switch med varje position och funktionsanrop till sliden, default är så ifall på nåogt sätt hamnar i värde som ej finns nollställa till ursprungsvariabel (0).
 
 
   while (1) {
@@ -105,19 +101,35 @@ int main(void){
         ms=0;
       }
       */
-     
+
       if ((key=keyscan())>=0) {   // ...Any key pressed?
         if (pKey==key) c++; else {c=0; pKey=key;}
         l88mem(0,lookUpTbl[key]+(c<<4));
 
-        if (lookUpTbl[key]==10){   // A (up)
-          if (mode==0){   // view mode logiken
-            if (vUsrPos<10){ // om positionen är heltal, dvs på huvudslide
+        if (lookUpTbl[key]==10){       // A (up)
+          if (vUsrPos==0 || rUsrPos==1){
+            modeSwap--;                // krävs 5 gåner, tills uppnår modeSwap == ModeSwitch}
+          }
+          if (modeSwap==MODESWITCH){
+            if (mode==0){
+              mode=1;
+              vUsrPos=0;
+              rUsrPos=1;
+              modeSwap=0;
+            } else {
+              mode=0;
+              vUsrPos=0;
+              rUsrPos=1;
+              modeSwap=0;
+            }
+          }
+          if (mode==0){                  // view mode logiken
+            if (vUsrPos<10){             // om positionen är heltal, dvs på huvudslide
               vUsrPos--;
-            } else {  // annars tillbaks till huvudslide
+            } else {                     // annars tillbaks till huvudslide
               vUsrPos=(vUsrPos/10);
             }
-          } else if (mode==1){   // edit mode
+          } else if (mode==1){           // edit mode logiken
             switch (rUsrPos){
               case 1     : break;
               //case 11    : /* första slide första ruta alfabetet(); */   break;
@@ -159,21 +171,21 @@ int main(void){
               case 152   : rUsrPos--;        break;
               default    : rUsrPos=1;
             }
-          } else if (mode!=0 && mode!=1){ // om skulle bli tokigt
+          } else if (mode!=0 && mode!=1){   // om skulle bli tokigt
             vUsrPos=0;
             rUsrPos=0;
             mode=0;
           }
         }
 
-        if (lookUpTbl[key]==11){   // B (choose)
-          if (mode==0){   // view mode logiken
-            if (vUsrPos<10){ // om positionen är heltal, dvs på huvudslide
+        if (lookUpTbl[key]==11){      // B (choose)
+          if (mode==0){               // view mode logiken
+            if (vUsrPos<10){          // om positionen är heltal, dvs på huvudslide
               vUsrPos=(vUsrPos*10);
-            } else {  // annars tillbaks till huvudslide
+            } else {                  // annars tillbaks till huvudslide
               vUsrPos=(vUsrPos/10);
             }
-          } else if (mode==1){   // edit mode logiken
+          } else if (mode==1){        // edit mode logiken
             switch (rUsrPos){
               case 1     : rUsrPos=11111;             break; /* OBS ENDAST DETTA UNDANTAG ANNARS TVÅ ST DEF. SOM SAMMA VÄRDE */
               //case 11    : /* första slide första ruta alfabetet(); */   break;
@@ -214,23 +226,23 @@ int main(void){
               case 151    : rUsrPos=1511;            break;
               //case 1511   : /* andra slide infoslide ruta alfabetet(); */ break;
               case 152    : rUsrPos=15;              break;
-              default    : rUsrPos=1;
+              default     : rUsrPos=1;
             }
-          } else if (mode!=0 && mode!=1){ // om skulle bli tokigt
+          } else if (mode!=0 && mode!=1){   // om skulle bli tokigt
             vUsrPos=0;
             rUsrPos=0;
             mode=0;
           }
         }
 
-        if (lookUpTbl[key]==12){   // C (down)
-          if (mode==0){   // view mode logiken
-            if (vUsrPos<10){ // om positionen är ental, dvs på huvudslide
+        if (lookUpTbl[key]==12){     // C (down)
+          if (mode==0){              // view mode logiken
+            if (vUsrPos<10){         // om positionen är ental, dvs på huvudslide
               vUsrPos++;
-            } else {  // annars tillbaks till huvudslide
+            } else {                 // annars tillbaks till huvudslide
               vUsrPos=(vUsrPos/10);
             }
-          } else if (mode==1){   // edit mode logiken
+          } else if (mode==1){       // edit mode logiken
             switch (rUsrPos){
               case 1     : rUsrPos++;             break;
               //case 11    : /* första slide första ruta alfabetet(); */   break;
@@ -273,7 +285,7 @@ int main(void){
               case 152   : break;
               default    : rUsrPos=1;
             }
-          } else if (mode!=0 && mode!=1){ // om skulle bli tokigt, onödig väll? -- då har ju i switchen?
+          } else if (mode!=0 && mode!=1){   // om skulle bli tokigt
             vUsrPos=0;
             rUsrPos=0;
             mode=0;
@@ -283,8 +295,7 @@ int main(void){
         if (mode==0){
           /* View Mode*/
           switch (vUsrPos) {
-            // varje case representerar en slide som representeras av en funktion.
-            case 0    : vSlide0();            break;
+            case 0    : vSlide0();            break;        // varje case representerar en slide som representeras av en funktion.
             case 1    : vSlide1();            break;
             case 10   : vSlide1Info();        break;
             case 2    : vSlide2();            break;
@@ -299,7 +310,7 @@ int main(void){
         if (mode==1){
           switch (rUsrPos) {
             case 1     : rSlide1();             break;
-            case 11111 : /* första slide första ruta alfabetet(); */   break; /* OBS ENDAST DETTA UNDANTAG ANNARS TVÅ ST DEF. SOM SAMMA VÄRDE */
+            case 11111 : /* första slide första ruta alfabetet(); */   break;   /* OBS ENDAST DETTA UNDANTAG -- PGA TVÅ ST DEF. SOM SAMMA VÄRDE */
             case 2     : rSlide2();             break;
             case 21    : /* första slide andra ruta alfabetet(); */    break;
             case 3     : rSlide3();             break;
@@ -342,6 +353,10 @@ int main(void){
         }
 
         l88mem(2, vUsrPos);
+        l88mem(3, rUsrPos);
+        l88mem(4, 0);
+        l88mem(6, 0);
+        l88mem(7, modeSwap);
      
       }
     }
@@ -363,6 +378,7 @@ void vSlide1(void){
   LCD_ShowStr(10, 10, "Bloodtype", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Blood medicine", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 3", BLACK, TRANSPARENT);
+  LCD_ShowStr(110, 60, "* info", BLACK, TRANSPARENT);
 }
 
 void vSlide1Info(void){
@@ -370,6 +386,7 @@ void vSlide1Info(void){
   LCD_ShowStr(10, 10, "This is", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Slide 1", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "info-slide", BLACK, TRANSPARENT);
+  LCD_ShowStr(110, 60, "* back", BLACK, TRANSPARENT);
 }
 
 void vSlide2(void){
@@ -377,6 +394,7 @@ void vSlide2(void){
   LCD_ShowStr(10, 10, "Something 4", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Something 5", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 6", BLACK, TRANSPARENT);
+  LCD_ShowStr(110, 60, "* info", BLACK, TRANSPARENT);
 }
 
 void vSlide2Info(void){
@@ -384,6 +402,7 @@ void vSlide2Info(void){
   LCD_ShowStr(10, 10, "This is ", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "slide 2", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "info-slide", BLACK, TRANSPARENT);
+  LCD_ShowStr(110, 60, "* back", BLACK, TRANSPARENT);
 }
 
 void vSlide3(void){
@@ -391,6 +410,7 @@ void vSlide3(void){
   LCD_ShowStr(10, 10, "Something 7", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Something 8", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 9", BLACK, TRANSPARENT);
+  LCD_ShowStr(110, 60, "* info", BLACK, TRANSPARENT);
 }
 
 void vSlide3Info(void){
@@ -398,10 +418,11 @@ void vSlide3Info(void){
   LCD_ShowStr(10, 10, "This is", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "slide 3", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "info-slide", BLACK, TRANSPARENT);
+  LCD_ShowStr(110, 60, "* back", BLACK, TRANSPARENT);
 }
 
 /* funktionsdefinitioner Edit(r)-mode */
-//vit börjar
+// Vit börjar
 void rSlide1(void){
   LCD_Clear(WHITE);
   LCD_ShowStr(10, 10, "* Alan Bortoleto", BLACK, TRANSPARENT);
@@ -429,13 +450,13 @@ void rSlide3(void){
 
 //rSlide30 (tillskrivning via alfabetet)
 
-/* röd börjar */
+// Röd börjar
 void rSlide4(void){
   LCD_Clear(RED);
   LCD_ShowStr(10, 10, "* Bloodtype", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Blood medicine", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 3", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide40 (tillskrivning via alfabetet)
@@ -445,7 +466,7 @@ void rSlide5(void){
   LCD_ShowStr(10, 10, "Bloodtype", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "* Blood medicine", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 3", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide50 (tillskrivning via alfabetet)
@@ -455,7 +476,7 @@ void rSlide6(void){
   LCD_ShowStr(10, 10, "Bloodtype", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Blood medicine", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "* Something 3", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide60 (tillskrivning via alfabetet)
@@ -473,7 +494,7 @@ void rSlide7Info(void){
   LCD_ShowStr(10, 10, "* This is", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Slide 1", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "info-slide", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "back", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "back", BLACK, TRANSPARENT);
 }
 
 //rSlide700 (tillskrivning via alfabetet)
@@ -486,13 +507,13 @@ void rSlide7InfoB(void){
   LCD_ShowStr(110, 60, "* back", BLACK, TRANSPARENT);
 }
 
-/* orange börjar */
+// Orange börjar
 void rSlide8(void){
   LCD_Clear(BRRED);
   LCD_ShowStr(10, 10, "* Something 4", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Something 5", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 6", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide80 (tillskrivning via alfabetet)
@@ -502,7 +523,7 @@ void rSlide9(void){
   LCD_ShowStr(10, 10, "Something 4", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "* Something 5", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 6", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide90 (tillskrivning via alfabetet)
@@ -512,7 +533,7 @@ void rSlide10(void){
   LCD_ShowStr(10, 10, "Something 4", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Something 5", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "* Something 6", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide100 (tillskrivning via alfabetet)
@@ -530,7 +551,7 @@ void rSlide11Info(void){
   LCD_ShowStr(10, 10, "* This is ", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "slide 2", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "info-slide", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "back", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "back", BLACK, TRANSPARENT);
 }
 
 //rSlide1100 (tillskrivning via alfabetet)
@@ -543,13 +564,13 @@ void rSlide11InfoB(void){
   LCD_ShowStr(110, 60, "* back", BLACK, TRANSPARENT);
 }
 
-/* gul börjar */
+// Gul börjar
 void rSlide12(void){
   LCD_Clear(YELLOW);
   LCD_ShowStr(10, 10, "* Something 7", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Something 8", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 9", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide120 (tillskrivning via alfabetet)
@@ -559,7 +580,7 @@ void rSlide13(void){
   LCD_ShowStr(10, 10, "Something 7", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "* Something 8", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "Something 9", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide130 (tillskrivning via alfabetet)
@@ -569,7 +590,7 @@ void rSlide14(void){
   LCD_ShowStr(10, 10, "Something 7", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "Something 8", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "* Something 9", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "info", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "info", BLACK, TRANSPARENT);
 }
 
 //rSlide1400 (tillskrivning via alfabetet)
@@ -587,7 +608,7 @@ void rSlide15Info(void){
   LCD_ShowStr(10, 10, "* This is", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 30, "slide 3", BLACK, TRANSPARENT);
   LCD_ShowStr(10, 50, "info-slide", BLACK, TRANSPARENT);
-  LCD_ShowStr(110, 60, "back", BLACK, TRANSPARENT);
+  LCD_ShowStr(120, 60, "back", BLACK, TRANSPARENT);
 }
 
 //rSlide1500 (tillskrivning via alfabetet)
